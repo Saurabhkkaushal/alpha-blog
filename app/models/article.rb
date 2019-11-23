2cfe3f9db1220f24eab0c92260b263f11f28776b
Saurabh <saurabhkaushal17@gmail.com>
2019-11-23 12:07:29 +0530

    article model with validation

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+  validates :description, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 300 }
+end
